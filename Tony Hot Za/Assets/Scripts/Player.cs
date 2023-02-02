4cf1fa4ebe1d7372cd68b9d6e5a96eb53dba7f22
Matthew J Pearson <seaofcode@gmail.com>
2023-02-02 14:36:10 -0600

    Animations

--- Tony Hot Za/Assets/Scripts/Player.cs
+++ Tony Hot Za/Assets/Scripts/Player.cs
@@ -6,6 +6,8 @@ public class Player : MonoBehaviour
 {
     [SerializeField] private float moveSpeed = 7f;
 
+    private bool isWalking;
+
     private void Update()
     {
         Vector2 inputVector = new Vector2(0, 0);
@@ -28,8 +30,14 @@ public class Player : MonoBehaviour
         Vector3 moveDir = new Vector3(inputVector.x, 0f, inputVector.y);
         transform.position += moveDir * moveSpeed * Time.deltaTime;
         
+        isWalking = moveDir != Vector3.zero;
         float rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, rotateSpeed * Time.deltaTime);
-        Debug.Log(inputVector);
     }
+
+    public bool IsWalking(){
+        return isWalking;
+    }
+
+
 }

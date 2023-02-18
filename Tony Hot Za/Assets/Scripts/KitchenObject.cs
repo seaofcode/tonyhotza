e1470dda59bbe112e3dbdc6dcf8adb446cd6c1e7
Matthew J Pearson <seaofcode@gmail.com>
2023-02-18 17:30:53 -0600

    Kitchen Object Parent
    Counter knows if its empty or not, if not it knows what Scriptable Object is on it, Switching Interact options adds moving the option to another counter with a visual update.

---
+++ Tony Hot Za/Assets/Scripts/KitchenObject.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KitchenObject : MonoBehaviour
+{
+    [SerializeField] private KitchenObjectSO kitchenObjectSO;
+
+    private ClearCounter clearCounter;
+
+    public KitchenObjectSO GetKitchenObjectSO() { return kitchenObjectSO; }
+
+    public void SetClearCounter(ClearCounter clearCounter)
+    {
+        if (this.clearCounter != null) { 
+            this.clearCounter.ClearKitchenObject();
+        }
+
+        this.clearCounter = clearCounter;
+
+        if (clearCounter.HasKitchenObject()) {
+            Debug.Log("Kitchen already has a KO");
+        }
+
+        clearCounter.SetKitchenObject(this);
+
+        transform.parent = clearCounter.GetKitchenObjectFollowTransform();
+        transform.localPosition = Vector3.zero;
+    }
+
+    public ClearCounter GetClearCounter() { return clearCounter; }
+}

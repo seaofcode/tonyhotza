7de912442ea8502bcc029953ab87f42d4f6b1331
Matthew J Pearson <seaofcode@gmail.com>
2023-02-27 10:45:14 -0600

    Logic for picking up and placing items

--- Tony Hot Za/Assets/Scripts/ClearCounter.cs
+++ Tony Hot Za/Assets/Scripts/ClearCounter.cs
@@ -7,7 +7,32 @@ public class ClearCounter : BaseCounter
     [SerializeField] private KitchenObjectSO kitchenObjectSO;
 
     public override void Interact(Player player){
-        
+        if(!HasKitchenObject())
+        {
+            // There is no Kitchen Object
+            if (player.HasKitchenObject())
+            {
+                // Player is carrying item
+                player.GetKitchenObject().SetKitchenObjectParent(this);
+            }
+            else
+            {
+                // player has nothing
+            }
+        }
+        else
+        {
+            // There is a Kitchen Object
+            if (player.HasKitchenObject())
+            {
+                // player is carrying something
+            }
+            else
+            {
+                // player is not carrying anything
+                GetKitchenObject().SetKitchenObjectParent(player);
+            }
+        }
     }
 }
 
